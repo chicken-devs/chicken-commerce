d58b4110da3bf6fd2392f2f39a97234a5f73dcda
vietphamh <phamhoangviet88@gmail.com>
2021-08-09 09:20:48 +0700

    init App Structure.

---
+++ src/server/CKE.Modules.Account/Controllers/AccountsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace CKE.Modules.Account.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ILogger<AccountsController> _logger;
+
+        public AccountsController(ILogger<AccountsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<bool> Get()
+        {
+            return true;
+        }
+    }
+}
---
+++ src/server/CKE.Shared/Anchor.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CKE.Shared
+{
+    public class Anchor
+    {
+    }
+}
